54973811b90079421557d611e5c9064274e89807
Matt Richards <mrmattrichards@gmail.com>
2015-07-13 22:43:10 -0500

    Added skeleton for calendar

--- public/app.js
+++ public/app.js
@@ -14,10 +14,11 @@ export class App {
     config.title = 'The Minnesota Vikings Skol Line';
     config.map([
       {route: ['', 'home'],       name: 'home',        moduleId:'main'},
-      {route: 'music',            name: 'music',       moduleId:'music',   title:'Music',   nav:true},
-      {route: 'members',          name: 'members',     moduleId:'members', title:'Members', nav:true},
-      {route: 'gigs',             name: 'gigs',        moduleId:'gigs',    title:'Gigs',    nav:true},
+      {route: 'music',            name: 'music',       moduleId:'music',    title:'Music',   nav:true},
+      {route: 'members',          name: 'members',     moduleId:'members',  title:'Members', nav:true},
+      {route: 'gigs',             name: 'gigs',        moduleId:'gigs',     title:'Gigs',    nav:true},
       {route: 'gigDetail/:gigID', name: 'gigDetail',   moduleId:'gigDetail'},
+      {route: 'calendar',         name: 'calendar',    moduleId:'calendar', title:'Calendar', nav:true}
     ]);
   }
 
