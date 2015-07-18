bcdbaf2a43c701843f959705bf5c3e3e2c831eda
Matt Richards <mrmattrichards@gmail.com>
2015-07-18 07:50:34 -0500

    Improved music detail

--- app/models/music.js
+++ app/models/music.js
@@ -2,7 +2,10 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var MusicSchema = new Schema({
-  name: String
+  name: String,
+  sounds: [{description: String, url: String}],
+  music: [{description: String, url: String}],
+  drill: [{description: String, url: String}]
 });
 
 module.exports = mongoose.model('Music', MusicSchema);
